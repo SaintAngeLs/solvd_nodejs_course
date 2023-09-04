6ea1399817b91b36bf242cd6dcbfcdb28d824535
A.Voznesenskyi <voznesenskijandrej5@gmail.com>
2023-09-04 16:49:08 +0200

    updating the tests for the solution with callbacks

--- tasks/task8/test/__tests__/task8_TDD.ts
+++ tasks/task8/test/__tests__/task8_TDD.ts
@@ -8,46 +8,80 @@ describe('AsyncOperationManager', () => {
     manager = new AsyncOperationManager();
   });
 
-  test('should execute simulateAsyncOperation with a given delay', async () => {
-    const delay = 1000; 
-    const result = await manager.simulateAsyncOperation(delay);
-    expect(result).toBe(`Async operation completed after ${delay}ms and microtask executed.`);
+  test('should execute simulateAsyncOperation with a given delay', (done) => {
+    const delay = 1000;
+    manager.simulateAsyncOperation(delay, (result) => {
+      expect(result).toBe(`Async operation completed after ${delay}ms and microtask executed.`);
+      done();
+    });
   });
 
-  test('should execute simulateAsyncOperation with a given delay', async () => {
-    const delay = 100; 
-    const result = await manager.simulateAsyncOperation(delay);
-    expect(result).toBe(`Async operation completed after ${delay}ms and microtask executed.`);
-  });
-  test('should execute simulateAsyncOperation with a given delay', async () => {
+ 
+  test('should execute simulateAsyncOperation with a given delay', (done) => {
     const delay = 10; 
-    const result = await manager.simulateAsyncOperation(delay);
-    expect(result).toBe(`Async operation completed after ${delay}ms and microtask executed.`);
+    manager.simulateAsyncOperation(delay, (result) => {
+      expect(result).toBe(`Async operation completed after ${delay}ms and microtask executed.`);
+      done();
+    });
   });
 
-  test('should execute scheduleImmediate', async () => {
-    const result = await manager.scheduleImmediate();
-    expect(result).toBe('Immediate task executed.');
+  test('should execute scheduleImmediate', (done) => {
+    manager.scheduleImmediate((result) => {
+      expect(result).toBe('Immediate task executed.');
+      done();
+    });
   });
 
-  test('should execute microtasks before setImmediate', async () => {
-    const result1 = await manager.simulateAsyncOperation(100);
-    const result2 = await manager.scheduleImmediate();
-    expect(result1).toBe(`Async operation completed after 100ms and microtask executed.`);
-    expect(result2).toBe('Immediate task executed.');
+  test('the case in which execute microtasks before setImmediate', (done) => {
+    let completedMicrotask = false;
+    let completedImmediate = false;
+
+    manager.simulateAsyncOperation(100, (result) => {
+      completedMicrotask = true;
+      expect(result).toBe(`Async operation completed after 100ms and microtask executed.`);
+      if(completedImmediate) 
+        done();
+        
+    })
+
+    manager.scheduleImmediate((result) => {
+      completedImmediate = true;
+      expect(result).toBe('Immediate task executed.')
+      if (completedMicrotask)
+        done();
+    })
+
   });
 
-  test('should execute in correct sequence when simulateAsyncOperation and scheduleImmediate are mixed', async () => {
-    const delays = [100, 50];
-    const [result1, result2, result3] = await Promise.all([
-      manager.simulateAsyncOperation(delays[0]),
-      manager.simulateAsyncOperation(delays[1]),
-      manager.scheduleImmediate()
-    ]);
+  test('should execute in correct sequence when simulateAsyncOperation and scheduleImmediate are mixed', (done) => {
+    const delays_set = [100, 50];
+    
+    let completedCount = 0;
 
-    expect(result1).toBe(`Async operation completed after ${delays[0]}ms and microtask executed.`);
-    expect(result2).toBe(`Async operation completed after ${delays[1]}ms and microtask executed.`);
-    expect(result3).toBe('Immediate task executed.');
+    function checkCompletionStatus()
+    {
+      completedCount++;
+      if(completedCount == 3)
+      {
+        done();
+      }
+    }
+
+    manager.simulateAsyncOperation(delays_set[0], (result) => {
+      expect(result).toBe(`Async operation completed after 100ms and microtask executed.`);
+      checkCompletionStatus();
+    })
+
+    manager.simulateAsyncOperation(delays_set[1], (result) => {
+      expect(result).toBe(`Async operation completed after 50ms and microtask executed.`);
+      checkCompletionStatus();
+    })
+
+    manager.scheduleImmediate((result) => {
+      expect(result).toBe('Immediate task executed.');
+      checkCompletionStatus();
+    })
   });
+ 
 
 });
