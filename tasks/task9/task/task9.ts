c06531af336c32677b7c5bb7b1545e1d8c7e84ac
A.Voznesenskyi <voznesenskijandrej5@gmail.com>
2023-09-05 20:24:27 +0200

    adding the basic task implementation

---
+++ tasks/task9/task/task9.ts
@@ -0,0 +1,36 @@
+/**
+ * Class representing an individual Book.
+ * It contains properties like title, author, ISBN, price, and availability.
+ */
+export class Book {
+    //TODO: constructor and reduceAvailability method 
+
+}
+
+/**
+ * Class representing a User.
+ * It contains properties like name, email, and a unique user ID.
+ */
+export class User {
+    //TODO: constructor and reduceAvailability method
+}
+
+  
+/**
+ * Class representing a shopping Cart.
+ * It has methods to add books, remove books, and calculate the total price.
+*/
+export class Cart {
+    // TODO: addBook, removeBook, calculateTotalPrice
+}
+
+  
+/**
+ * Class representing an Order.
+ * It includes information about the user, books ordered, and the total price.
+ */
+export class Order {
+
+    // TODO: constructor and getTotalPrice method
+ 
+}
