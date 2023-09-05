3466e2a7709b225fe702ba6f719545388e886ef1
A.Voznesenskyi <voznesenskijandrej5@gmail.com>
2023-09-05 20:41:05 +0200

    adding the full beginnning tests to the task 8

--- tasks/task9/test/__tests__/task9_TDD.ts
+++ tasks/task9/test/__tests__/task9_TDD.ts
@@ -5,24 +5,28 @@ import {
 describe('Book class tests', () => {
 
     it('should initialize book objects correctly', () => {
-        const title = "Polymorphic Objects and Dynamic Projecting Patterns";
-        const author = "Alexandrina Poida";
-        const ISBN = "311415926538598";
-        const price = 400.99;
+        const bookName = "Polymorphic Objects and Dynamic Projecting Patterns";
+        const bookAuthor = "Alexandrina Poida";
+        const bookId = "311415926538598";
+        const bookPrice = 400.99;
         const availability = 10;
     
-        const book = new Book(title, author, ISBN, price, availability);
+        const book = new Book(bookName, bookAuthor, bookId, bookPrice, availability);
     
-        expect(book.title).toBe(title);
-        expect(book.author).toBe(author);
-        expect(book.ISBN).toBe(ISBN);
-        expect(book.price).toBe(price);
+        expect(book.title).toBe(bookName);
+        expect(book.author).toBe(bookAuthor);
+        expect(book.ISBN).toBe(bookId);
+        expect(book.price).toBe(bookPrice);
         expect(book.availability).toBe(availability);
       });
 
     it('should reduct the acvailability when a book is ordered', () => {
         const numerOfAvailable = 10;
-        const book = new Book("Polimorhic objects and dynamic projecting patterns", "Alexandrina Poida", "311415926538598", 400.99, numerOfAvailable)
+        const bookName = "Polimorhic objects and dynamic projecting patterns";
+        const bookAuthor = "Alexandrina Poida";
+        const bookId = "311415926538598";
+        const bookPrice = "400.99";
+        const book = new Book(bookName, bookAuthor, bookId, bookPrice, numerOfAvailable);
         book.reduceAvailability(1);
         expect(book.acvailability).toBe(numerOfAvailable-1);
     })
@@ -38,7 +42,7 @@ describe('User class tests', () => {
     it('should correctly store user information', () => {
         const someUserName = "Alexandrina";
         const someuserEmail = "alexandrina@email.com";
-        const someUserId = "1"
+        const someUserId = "1";
         const user = new User(someUserName, someuserEmail, someUserId);
         expect(user.name).toBe(someUserName);
         expect(user.email).toBe(someuserEmail);
@@ -49,7 +53,7 @@ describe('User class tests', () => {
     it('should correctly store user information', () => {
         const someUserName = "Michael";
         const someuserEmail = "michael@email.com";
-        const someUserId = "2"
+        const someUserId = "2";
         const user = new User(someUserName, someuserEmail, someUserId);
         expect(user.name).toBe(someUserName);
         expect(user.email).toBe(someuserEmail);
@@ -57,3 +61,70 @@ describe('User class tests', () => {
     });
  });
 
+
+
+describe('Cart class tests', () => {
+    it('should correctly add books and calculate the total price', () => {
+
+        const numerOfAvailable = 10;
+        const bookName = "Polimorhic objects and dynamic projecting patterns";
+        const bookAuthor = "Alexandrina Poida";
+        const bookId = "311415926538598";
+        const bookPrice = "400.99";
+
+        const book = new Book(bookName, bookAuthor, bookId, bookPrice, numerOfAvailable)
+
+        const cart = new Cart();
+
+        cart.addBook(book, 2);
+  
+        expect(cart.calculateTotalPrice()).toBe(Number(bookPrice)*2);
+        expect(book.availability).toBe(numerOfAvailable);
+    });
+  
+    it('should correctly remove books from the cart', () => {
+        const numerOfAvailable = 10;
+        const bookName = "Polimorhic objects and dynamic projecting patterns";
+        const bookAuthor = "Alexandrina Poida";
+        const bookId = "311415926538598";
+        const bookPrice = "400.99";
+
+        const book = new Book(bookName, bookAuthor, bookId, bookPrice, numerOfAvailable);
+
+        const cart = new Cart();
+        cart.addBook(book, 1);
+        cart.removeBook(book);
+  
+        expect(cart.books.length).toBe(0);
+    });
+});
+
+
+
+describe('Order class tests', () => {
+    it('should correctly create an order and get the total price', () => {
+    
+        const someUserName = "Michael";
+        const someuserEmail = "michael@email.com";
+        const someUserId = "2";
+
+        const user = new User(someUserName, someuserEmail, someUserId);
+
+
+        const numerOfAvailable = 10;
+        const bookName = "Polimorhic objects and dynamic projecting patterns";
+        const bookAuthor = "Alexandrina Poida";
+        const bookId = "311415926538598";
+        const bookPrice = "400.99";
+
+        const book = new Book(bookName, bookAuthor, bookId, bookPrice, numerOfAvailable);
+
+        const cart = new Cart();
+        cart.addBook(book, 1);
+  
+        const order = new Order(user, cart);
+  
+        expect(order.getTotalPrice()).toBe(Number(bookPrice));
+    });
+  });
+  
